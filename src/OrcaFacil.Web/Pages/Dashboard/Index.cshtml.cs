d2c843cebb40e855ec48956f746a0082babc90b5
MNSOFT <137189378+devmnsoft@users.noreply.github.com>
2026-08-01 09:16:40 -0300

    feat: evolve premium dashboard and account shell

--- src/OrcaFacil.Web/Pages/Dashboard/Index.cshtml.cs
+++ src/OrcaFacil.Web/Pages/Dashboard/Index.cshtml.cs
@@ -1,8 +1,5 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using OrcaFacil.Application.Abstractions;
-using OrcaFacil.Application.DTOs;
-using OrcaFacil.Application.Profile;
 using OrcaFacil.Web.Services;
 
 namespace OrcaFacil.Web.Pages.Dashboard;
@@ -10,28 +7,13 @@ namespace OrcaFacil.Web.Pages.Dashboard;
 [Authorize]
 public class IndexModel : PageModel
 {
-    private readonly ICurrentUserService _current;
-    private readonly IDashboardQueries _queries;
-    private readonly ProfileService _profiles;
-    private readonly INextBestActionService _nextBestAction;
-    public NextBestAction? NextAction { get; private set; }
-    public string FirstName => string.IsNullOrWhiteSpace(_current.Name) ? "bem-vindo" : _current.Name.Split(' ', StringSplitOptions.RemoveEmptyEntries)[0];
+    private readonly IDashboardExperienceService _experience;
+    public DashboardExperienceViewModel Experience { get; private set; } = null!;
 
-    public DashboardDto? Dashboard { get; private set; }
-    public bool HasIssuerProfile { get; private set; }
-
-    public IndexModel(ICurrentUserService current, IDashboardQueries queries, ProfileService profiles, INextBestActionService nextBestAction)
-    {
-        _current = current;
-        _queries = queries;
-        _profiles = profiles;
-        _nextBestAction = nextBestAction;
-    }
+    public IndexModel(IDashboardExperienceService experience) => _experience = experience;
 
     public async Task OnGetAsync(CancellationToken ct)
     {
-        Dashboard = await _queries.GetDashboardAsync(_current.UserId, ct);
-        HasIssuerProfile = await _profiles.GetAsync(new(_current.UserId), ct) is not null;
-        NextAction = await _nextBestAction.GetAsync(ct);
+        Experience = await _experience.GetAsync(ct);
     }
 }
